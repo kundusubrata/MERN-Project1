002746c9740917a781a99efd5acced5097690729
Subrata Kundu <kundu.subrata2020@outlook.com>
2024-01-03 21:40:14 +0530

    Day 5 Commit

--- Backend/Routes/productRoute.js
+++ Backend/Routes/productRoute.js
@@ -5,16 +5,21 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetails,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
 } = require("../Controllers/productController");
 const { isAuthenticatedUser ,authorizeRoles} = require("../Middleware/auth");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/products/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct);
-router.route("/products/:id")
+router.route("/admin/products/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct);
+router.route("/admin/products/:id")
   .put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct)
-  .delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct)
-  .get(getProductDetails);
+  .delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct);
+router.route("/product/:id").get(getProductDetails);
+router.route("/review").put(isAuthenticatedUser,createProductReview);
+router.route("/reviews").get(getProductReviews).delete(isAuthenticatedUser,deleteReview);
 
 module.exports = router;
